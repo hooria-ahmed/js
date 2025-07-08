de71411613b47e1864b793c4560c7b497cc1cdbe
Hooria Ahmed <hooria.2006.haq@gmail.com>
2025-07-08 07:03:07 +0000

    Variables and constants done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hooria.2006.haq@gmail.com"
+var accountPassword = "12345" // don't use 'var' due to block space issue.
+accountCity = "Karachi"
+
+// accountId = 2 //not allowed!, bcz 'accounId' is declared constant previously.
+
+
+accountEmail = "random@gmail.com"
+accountPassword = "2121212"
+accountCity = "Islamabad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
