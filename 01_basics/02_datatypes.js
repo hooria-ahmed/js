5fa827ffef99bd0796017479941048cbef7b194d
Hooria Ahmed <hooria.2006.haq@gmail.com>
2025-07-08 12:02:32 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3 )// we are using nodejs, not browser
+
+let name = "Hooria"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// DATA TYPES(Primitive)
+// number => 2 to power 53
+// bigint is used if values are very large
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value nor defined yet
+// symbol => showcase uniqueness of any particular value
+
+// DATA TYPES()
+// object
+
+console.log(typeof "hooria");
+
