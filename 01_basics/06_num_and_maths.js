a8a53cb26b015a82a0c6a874d6d7f94dfcb48705
Hooria Ahmed <hooria.2006.haq@gmail.com>
2025-07-12 09:40:27 +0000

    Number and Maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,44 @@
+//  ++++++++++++++++++++++++++++ Number +++++++++++++++++++++++++
+// Declaring a number, two ways
+const score = 400
+const score_02 = new Number(100)
+
+// typecasting number into string
+
+console.log(score_02.toString());
+
+// Adding decimal places
+console.log(balance.toFixed(2));
+
+// Precesion
+const otherNumber = 12.35677
+console.log(otherNumber.toPrecision(4))
+
+// Locale String, represents number in US way of writing number
+const Hundreds = 1000000
+console.log(Hundreds.toLocaleString());
+
+// +++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-9));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.9));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 9, 3, 7));
+console.log(Math.max(4, 9, 3, 7));
+
+
+console.log(Math.random());
+
+
+
+
+
+
+
+
+
+
+
+
